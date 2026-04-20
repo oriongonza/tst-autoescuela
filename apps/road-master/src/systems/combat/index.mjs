9e5337eb1f9fea1baff617c37630ca8c9dbbdfeb
Orion "Ardi" Gonzalez <oriongonza@gmail.com>
2026-04-20 23:19:06 +0200

    Implement combat and pacing engine slice (#70)

---
+++ apps/road-master/src/systems/combat/index.mjs
@@ -0,0 +1,15 @@
+export {
+  DEFAULT_ATTACK_LIBRARY,
+  DEFAULT_BOSS_PHASE_PLAN,
+} from "./constants.mjs";
+
+export {
+  createCombatEncounter,
+  getCombatSnapshot,
+  resolveCombatTurn,
+} from "./engine.mjs";
+
+export {
+  runCombatPacingSimulation,
+} from "./simulation.mjs";
+
